e05939d2d54574f090f82e9b560d54316e63e4be
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-07-26 19:41:33 +0530

    create seperate function for category based dishes api

--- src/services/dishes.service.js
+++ src/services/dishes.service.js
@@ -1,10 +1,53 @@
 import db from "../config/db.js";
 
 const getAllDishes = async ()=> {
-    const [dishes] = await db.query("SELECT* From dishes");
+    const [dishes] = await db.execute("SELECT * From dishes");
+    return dishes;
+}
+
+// 'Indian','Italian','Mexican','Chinese','Korean','Japanese'
+
+const getIndianDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Indian'`);
+    return dishes;
+}
+
+
+const getItalianDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Italian'`);
+    return dishes;
+}
+
+
+const getMexicanDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Mexican'`);
+    return dishes;
+}
+
+
+const getChineseDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Chinese'`);
+    return dishes;
+}
+
+
+const getKoreanDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Korean'`);
+    return dishes;
+}
+
+
+const getJapaneseDishes = async ()=> {
+    const [dishes] = await db.execute(`SELECT * FROM dishes WHERE category = 'Japanese'`);
     return dishes;
 }
 
 export default {
     getAllDishes,
+    getIndianDishes,
+    getItalianDishes,
+    getMexicanDishes,
+    getChineseDishes,
+    getKoreanDishes,
+    getJapaneseDishes,
 };
