d65438910bee4d02280a46442407eb638a538f0a
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-07-26 19:42:36 +0530

    create seperate controller functions for the category based dishes api

--- src/controllers/dishes.controller.js
+++ src/controllers/dishes.controller.js
@@ -4,11 +4,83 @@ const getDishes = async (req, res) => {
     const dishes = await dishesService.getAllDishes();
 
     res.status(200).json({
-        success:true,
-        data:dishes,
+        success: true,
+        data: dishes,
     })
 }
 
-export default{
+    // getIndianDishes,
+    // getItalianDishes,
+    // getMexicanDishes,
+    // getChineseDishes,
+    // getKoreanDishes,
+    // getJapaneseDishes,
+
+const getIndianDishes = async (req, res) => {
+    const dishes = await dishesService.getIndianDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+
+const getItalianDishes = async (req, res) => {
+    const dishes = await dishesService.getItalianDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+
+const getMexicanDishes = async (req, res) => {
+    const dishes = await dishesService.getMexicanDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+
+const getChineseDishes = async (req, res) => {
+    const dishes = await dishesService.getChineseDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+
+const getKoreanDishes = async (req, res) => {
+    const dishes = await dishesService.getKoreanDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+
+const getJapaneseDishes = async (req, res) => {
+    const dishes = await dishesService.getJapaneseDishes();
+
+    res.status(200).json({
+        success: true,
+        data: dishes,
+    })
+}
+
+export default {
     getDishes,
+    getIndianDishes,
+    getItalianDishes,
+    getMexicanDishes,
+    getChineseDishes,
+    getKoreanDishes,
+    getJapaneseDishes,
 }
