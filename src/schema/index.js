15cea66209f6623ae46e23837dc5f481cb5847f0
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-07-26 18:42:47 +0530

    added the import and calls of the new schema in the index.js

--- src/schema/index.js
+++ src/schema/index.js
@@ -4,6 +4,8 @@ import createLocationsTable  from "./locations.schema.js";
 import createRestaurantTables  from "./tables.schema.js";
 import createOrdersTable  from "./orders.schema.js";
 import createOrderItemsTable from "./orderItems.schema.js";
+import createCartsTable from "./cart.schema.js";
+import createCartItemsTable from "./cartItems.schema.js";
 
 const initializeDatabase = async () => {
     try {
@@ -15,8 +17,10 @@ const initializeDatabase = async () => {
         await createRestaurantTables();
         await createOrdersTable();
         await createOrderItemsTable();
+        await createCartsTable();
+        await createCartItemsTable();
 
-        // console.log("🎉 Database initialized successfully");
+        
     } catch (err) {
         console.error("Database initialization failed");
         console.error(err);
