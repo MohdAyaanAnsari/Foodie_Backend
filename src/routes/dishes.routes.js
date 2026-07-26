ce995676272efb73bf7c5f21851f1fc8f0b481fc
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-07-26 19:43:44 +0530

    created seperate routes for the cetgory based dishes api

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -4,5 +4,11 @@ const router = express.Router();
 
 
 router.get("/AllDishes", dishesController.getDishes);
+router.get("/IndianDishes", dishesController.getIndianDishes);
+router.get("/ItalianDishes", dishesController.getItalianDishes);
+router.get("/MexicanDishes", dishesController.getMexicanDishes);
+router.get("/ChineseDishes", dishesController.getChineseDishes);
+router.get("/KoreanDishes", dishesController.getKoreanDishes);
+router.get("/JapaneseDishes", dishesController.getJapaneseDishes);
 
 export default router;
