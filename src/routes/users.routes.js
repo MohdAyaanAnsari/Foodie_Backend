3d7064a538846525f2463e7f2f4a47d19c46e8e8
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-07 10:52:52 +0530

    add the route for the create user

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import usercontroller from "../controllers/user.controller.js";
 
 router.get("/AllUsers", usercontroller.getUsers);
+router.post("/createUser", usercontroller.createUser);
 
 export default router;
