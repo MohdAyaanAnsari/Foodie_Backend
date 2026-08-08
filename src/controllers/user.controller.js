9af1284dd7486bae0abbc825299bbacc86c94ad8
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-08 22:50:25 +0530

    commented create user from the user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,24 +10,24 @@ const getUsers = async(req, res) => {
     });
 }
 
-const createUser = async(req, res) =>{
-    try{
+// const createUser = async(req, res) =>{
+//     try{
 
-        const user = await userService.createUser(req.body);
+//         const user = await userService.createUser(req.body);
         
-        res.status(201).json({
-            success:true,
-            data:user,
-        })
-    }catch(error){
-        res.status(500).json({
-            success:false,
-            message:error.message,
-        })
-    }
-}
+//         res.status(201).json({
+//             success:true,
+//             data:user,
+//         })
+//     }catch(error){
+//         res.status(500).json({
+//             success:false,
+//             message:error.message,
+//         })
+//     }
+// }
 
 export default {
     getUsers,
-    createUser
+    // createUser
 }
