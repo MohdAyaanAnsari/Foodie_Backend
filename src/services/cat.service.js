96a18a67a3202068e5dbad394bcbc7f9665998b9
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-17 19:30:54 +0530

    updated the image path to serve them

--- src/services/cat.service.js
+++ src/services/cat.service.js
@@ -5,37 +5,37 @@ const categoryMeta = {
     description:
       "Authentic Indian cuisine featuring rich curries, aromatic biryanis, and traditional tandoori specialties.",
     image:
-      "/indian.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_indian.png",
   },
   Italian: {
     description:
       "Classic Italian dishes including handcrafted pasta, wood-fired pizzas, and creamy risottos.",
     image:
-      "/italian.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_italian.png",
   },
   Mexican: {
     description:
       "Bold Mexican flavors with tacos, burritos, quesadillas, and freshly prepared salsas.",
     image:
-      "/mexican.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_mexicon.png",
   },
   Chinese: {
     description:
       "Traditional Chinese favorites including noodles, fried rice, dumplings, and stir-fried delicacies.",
     image:
-      "/chinese.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_chinese.png",
   },
   Korean: {
     description:
       "Popular Korean dishes featuring BBQ, kimchi, bibimbap, and spicy street-food favorites.",
     image:
-      "/korean.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_korean.png",
   },
   Japanese: {
     description:
       "Fresh Japanese cuisine including sushi, ramen, tempura, and premium seafood specialties.",
     image:
-      "/japanese.png",
+      "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/cat_japanese.png",
   },
 };
 
