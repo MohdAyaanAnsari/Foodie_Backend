ec4815e281399b3e1ec879991ba12f4bd5493e84
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-22 10:17:08 +0530

    added new route

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 import cartController from "../controllers/cart.controller.js";
 
 router.get('/AllCarts', cartController.getCarts);
+router.post("/add", cartController.addToCart);
 
 export default router;
