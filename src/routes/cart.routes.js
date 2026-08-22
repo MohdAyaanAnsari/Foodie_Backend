90040fb771c801ce0dd47690ee42ace6b0d17611
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-22 10:20:12 +0530

    added new route

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -5,5 +5,6 @@ import cartController from "../controllers/cart.controller.js";
 
 router.get('/AllCarts', cartController.getCarts);
 router.post("/add", cartController.addToCart);
+router.get("/:user_id", cartController.getUserCart);
 
 export default router;
