e16577f33904743b1369e39350491a63e2448be8
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-23 10:01:42 +0530

    updated the controller by fixing thigs at updating cart items

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,13 +1,24 @@
 import cartService from "../services/cart.service.js";
 
-const getCarts = async(req, res) =>{
-    const carts = await cartService.getAllCarts();
 
-    res.status(200).json({
-        success: true,
-        data: carts,
-    })
-}
+const getCarts = async (req, res) => {
+    try {
+        const carts = await cartService.getAllCarts();
+
+        res.status(200).json({
+            success: true,
+            data: carts,
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+};
 
 
 const addToCart = async (req, res) => {
@@ -41,7 +52,7 @@ const addToCart = async (req, res) => {
             message: err.message,
         });
     }
-}
+};
 
 
 const getUserCart = async (req, res) => {
@@ -54,6 +65,7 @@ const getUserCart = async (req, res) => {
             success: true,
             data: cart,
         });
+
     } catch (err) {
         console.error(err);
 
@@ -65,8 +77,78 @@ const getUserCart = async (req, res) => {
 };
 
 
-export default{
+/*
+|--------------------------------------------------------------------------
+| UPDATE CART ITEM QUANTITY
+|--------------------------------------------------------------------------
+*/
+
+const updateCartItem = async (req, res) => {
+    try {
+        const { cart_item_id } = req.params;
+        const { quantity } = req.body;
+
+        if (!quantity || quantity < 1) {
+            return res.status(400).json({
+                success: false,
+                message: "Quantity must be at least 1",
+            });
+        }
+
+        const result = await cartService.updateCartItem(
+            cart_item_id,
+            quantity
+        );
+
+        res.status(200).json({
+            success: true,
+            message: "Cart quantity updated",
+            data: result,
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+};
+
+
+/*
+|--------------------------------------------------------------------------
+| REMOVE CART ITEM
+|--------------------------------------------------------------------------
+*/
+
+const removeCartItem = async (req, res) => {
+    try {
+        const { cart_item_id } = req.params;
+
+        await cartService.removeCartItem(cart_item_id);
+
+        res.status(200).json({
+            success: true,
+            message: "Item removed from cart",
+        });
+
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+};
+
+
+export default {
     getCarts,
     addToCart,
     getUserCart,
-}
+    updateCartItem,
+    removeCartItem,
+};
