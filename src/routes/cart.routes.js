ffaa16f0d05494667645fd8430b1a502be61e4f9
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-23 10:02:58 +0530

    added route for updating the cat items

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,10 +1,22 @@
 import express from "express";
-
-const router = express.Router();
 import cartController from "../controllers/cart.controller.js";
 
-router.get('/AllCarts', cartController.getCarts);
+const router = express.Router();
+
+router.get("/AllCarts", cartController.getCarts);
+
 router.post("/add", cartController.addToCart);
+
 router.get("/:user_id", cartController.getUserCart);
+
+router.patch(
+    "/item/:cart_item_id",
+    cartController.updateCartItem
+);
+
+router.delete(
+    "/item/:cart_item_id",
+    cartController.removeCartItem
+);
 
 export default router;
