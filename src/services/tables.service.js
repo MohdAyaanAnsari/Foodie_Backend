3aa2d802c510fb103330392ad5e9da8c384a3ffb
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-08-26 18:12:32 +0530

    added the image with the response of find table api

--- src/services/tables.service.js
+++ src/services/tables.service.js
@@ -1,5 +1,17 @@
 import db from "../config/db.js";
 
+const TABLE_TYPE_IMAGES = {
+  Silver: "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/table/Silver.png",
+  Gold: "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/table/Gold.png",
+  VIP: "https://raw.githubusercontent.com/MohdAyaanAnsari/Foodie_Photos/refs/heads/main/table/Vip.png",
+};
+
+const attachImage = (tables) =>
+  tables.map((table) => ({
+    ...table,
+    image: TABLE_TYPE_IMAGES[table.type] || "/images/tables/default.jpg",
+  }));
+
 /**
  * Get every restaurant table.
  *
@@ -30,65 +42,53 @@ const getAllTables = async () => {
  * It only returns tables that are currently available.
  */
 const findAvailableTables = async ({
+  reservationDate,
+  reservationTime,
+  guests,
+  tableType,
+}) => {
+  const sql = `
+    SELECT
+      rt.id,
+      rt.table_number,
+      rt.seats,
+      rt.type
+    FROM restaurant_tables rt
+    WHERE
+      rt.seats >= ?
+      AND rt.type = ?
+      AND NOT EXISTS (
+        SELECT 1
+        FROM table_reservations tr
+        WHERE
+          tr.table_id = rt.id
+          AND tr.reservation_date = ?
+          AND tr.status IN ('Pending', 'Confirmed')
+          AND ? < ADDTIME(
+            tr.reservation_time,
+            SEC_TO_TIME(tr.duration_minutes * 60)
+          )
+          AND ADDTIME(
+            ?,
+            SEC_TO_TIME(120 * 60)
+          ) > tr.reservation_time
+      )
+    ORDER BY
+      rt.seats ASC,
+      rt.table_number ASC
+  `;
+
+  const [tables] = await db.execute(sql, [
+    guests,
+    tableType,
     reservationDate,
     reservationTime,
-    guests,
-    tableType
-}) => {
+    reservationTime,
+  ]);
 
-    const sql = `
-        SELECT
-            rt.id,
-            rt.table_number,
-            rt.seats,
-            rt.type
-
-        FROM restaurant_tables rt
-
-        WHERE
-            rt.seats >= ?
-            AND rt.type = ?
-
-            AND NOT EXISTS (
-
-                SELECT 1
-
-                FROM table_reservations tr
-
-                WHERE
-                    tr.table_id = rt.id
-
-                    AND tr.reservation_date = ?
-
-                    AND tr.status IN ('Pending', 'Confirmed')
-
-                    AND ? < ADDTIME(
-                        tr.reservation_time,
-                        SEC_TO_TIME(tr.duration_minutes * 60)
-                    )
-
-                    AND ADDTIME(
-                        ?,
-                        SEC_TO_TIME(120 * 60)
-                    ) > tr.reservation_time
-
-            )
-
-        ORDER BY
-            rt.seats ASC,
-            rt.table_number ASC
-    `;
-
-    const [tables] = await db.execute(sql, [
-        guests,
-        tableType,
-        reservationDate,
-        reservationTime,
-        reservationTime
-    ]);
-
-    return tables;
+  return attachImage(tables);
 };
+
 
 
 export default {
