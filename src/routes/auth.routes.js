13b33df59d3ffef73136ec3825f97c673ed8ae42
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-01 10:01:25 +0530

    added register and verify otp routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 import authController from "../controllers/auth.controller.js";
 
-router.post("/Signup/", authController.signup);
-router.get("/Signup/", authController.signupget);
+router.post("/Signup", authController.signup);
+router.post("/VerifyOtp", authController.verifyOtp);
 
 export default router;
