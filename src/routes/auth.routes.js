19b1ba53e9d7c151572a7b398b7a605740dd2583
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-02 18:08:05 +0530

    added new route for the login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,5 +5,6 @@ import authController from "../controllers/auth.controller.js";
 
 router.post("/Signup", authController.signup);
 router.post("/VerifyOtp", authController.verifyOtp);
+router.post("/login", authController.login);
 
 export default router;
