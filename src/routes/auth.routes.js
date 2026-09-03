1c8f5ba0630e5c885c6c166738c4663ca44d44f3
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-03 11:09:04 +0530

    added new routes for logout and me api

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,14 @@
 import express from "express";
 const router = express.Router();
+import authMiddleware from "../middleware/auth.middleware.js";
 
 import authController from "../controllers/auth.controller.js";
 
 router.post("/Signup", authController.signup);
 router.post("/VerifyOtp", authController.verifyOtp);
 router.post("/login", authController.login);
+router.post("/logout", authMiddleware, authController.logout);
+
+router.get("/me", authMiddleware, authController.me);
 
 export default router;
