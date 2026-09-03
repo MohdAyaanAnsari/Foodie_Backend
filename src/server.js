f8860924ddc67d36be3ac71ab4a3e2897d70c8b4
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-03 11:09:48 +0530

    implemented the cookie parser at server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 
 
@@ -22,6 +23,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(cors({
   origin: process.env.FrontEndUrl,
