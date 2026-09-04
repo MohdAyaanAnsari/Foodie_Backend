3cf6a6b7df74ed294157c7b046994fdbef14ed6a
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-04 09:59:22 +0530

    protected the api using middleware

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import dishesController from "../controllers/dishes.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 const router = express.Router();
 
 
-router.get("/AllDishes", dishesController.getDishes);
-router.get("/IndianDishes", dishesController.getIndianDishes);
-router.get("/ItalianDishes", dishesController.getItalianDishes);
-router.get("/MexicanDishes", dishesController.getMexicanDishes);
-router.get("/ChineseDishes", dishesController.getChineseDishes);
-router.get("/KoreanDishes", dishesController.getKoreanDishes);
-router.get("/JapaneseDishes", dishesController.getJapaneseDishes);
+router.get("/AllDishes", authMiddleware, dishesController.getDishes);
+router.get("/IndianDishes", authMiddleware, dishesController.getIndianDishes);
+router.get("/ItalianDishes", authMiddleware, dishesController.getItalianDishes);
+router.get("/MexicanDishes", authMiddleware, dishesController.getMexicanDishes);
+router.get("/ChineseDishes", authMiddleware, dishesController.getChineseDishes);
+router.get("/KoreanDishes", authMiddleware, dishesController.getKoreanDishes);
+router.get("/JapaneseDishes", authMiddleware, dishesController.getJapaneseDishes);
 
 export default router;
