961a43d5084cca4dbf8ab260f357105202d956c1
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-04 09:59:04 +0530

    protected the api using middleware

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -1,22 +1,23 @@
 import express from "express";
 import cartController from "../controllers/cart.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/AllCarts", cartController.getCarts);
+router.get("/AllCarts",authMiddleware, cartController.getCarts);
 
-router.post("/add", cartController.addToCart);
+router.post("/add",authMiddleware, cartController.addToCart);
 
-router.get("/:user_id", cartController.getUserCart);
+router.get("/getCart",authMiddleware, cartController.getUserCart);
 
 router.patch(
     "/item/:cart_item_id",
-    cartController.updateCartItem
+    authMiddleware, cartController.updateCartItem
 );
 
 router.delete(
     "/item/:cart_item_id",
-    cartController.removeCartItem
+    authMiddleware, cartController.removeCartItem
 );
 
 export default router;
