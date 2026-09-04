77ccf334c37370040f37998600daf3f356bf2ede
MohdAyaanAnsari <ayaanansari9997843139@gmail.com>
2026-09-04 09:59:57 +0530

    protected the api using middleware

--- src/routes/orders.routes.js
+++ src/routes/orders.routes.js
@@ -1,7 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-import orderController from "../controllers/orders.controller.js";
 import ordersController from "../controllers/orders.controller.js";
 
 router.get("/AllOrders", ordersController.getOrders);
